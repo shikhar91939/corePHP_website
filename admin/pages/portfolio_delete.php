e92b6277bcb05f458d9119ca3a6e80a755213187
shikhar91939 <shikhar.91939@gmail.com>
2016-06-16 15:45:08 +0530

    added delete functionality for portfolio items

--- admin/pages/portfolio_delete.php
+++ admin/pages/portfolio_delete.php
@@ -1,1 +1,10 @@
-delete
+<?php 
+include '../../connect.php';
+
+$id = $_REQUEST['id'];
+
+$query_delete = "DELETE FROM `portfolio_items` WHERE `portfolio_items`.`id` = $id";
+mysql_query($query_delete) or die("Bad mysql query:$query_delete<br>Error: ".mysql_error());
+
+header("location:portfolio_view.php");
+ ?>
